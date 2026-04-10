475750931383d01b202c24b321f016b3f1d276cf
sabbir-bot-comunity <s33932334@gmail.com>
2026-04-10 16:26:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const login = require("./sabbir-fca");
 const fs = require("fs");
 const logger = require("./utils/log");
+const express = require('express');
+
+const app = express();
+const botName = "SABBIR CHAT BOT";
+
 
 const langFile = JSON.parse(fs.readFileSync("./languages/bn.json", "utf8"));
 
-// নামসহ মেসেজ পাঠানোর ফাংশন
+
 const getText = (path, ...args) => {
-  const botName = "SABBIR CHAT BOT"; // আপনার নাম
   let text = path.split(".").reduce((obj, key) => obj?.[key], langFile) || path;
   args.forEach((arg, i) => text = text.replace(`%${i + 1}`, arg));
   
-  // যদি এটা লগিন বা সিস্টেম মেসেজ না হয়, তবে নামের ট্যাগ যোগ করবে
   if (path.startsWith("commands")) {
       return `[ ${botName} ] : ${text}`;
   }
   return text;
 };
 
+
+app.get('/', (req, res) => {
+  res.send(`${botName} IS RUNNING...`);
+});
+
+app.listen(3000, () => {
+  logger("Uptime Server রেডি (Port: 3000)!", "INFO");
+});
+
+
 module.exports = () => {
-  if (!fs.existsSync("./account.txt")) return logger(getText("login.noAppState"), "ERROR");
+  if (!fs.existsSync("./account.txt")) {
+    return logger(getText("login.noAppState"), "ERROR");
+  }
 
   const appState = JSON.parse(fs.readFileSync("./account.txt", "utf8"));
   
@@ -33,6 +48,7 @@ module.exports = () => {
 
       const message = event.body.toLowerCase();
 
+      
       if (message === "hi") {
         api.sendMessage(getText("commands.hi"), event.threadID);
       } 
@@ -42,13 +58,3 @@ module.exports = () => {
     });
   });
 };
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('SABBIR CHAT BOT IS RUNNING...');
-});
-
-app.listen(3000, () => {
-  logger("Uptime Server রেডি!", "INFO");
-});
