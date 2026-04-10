0fbb2621f5ed2102546eb9dfdae26ba63ed020a0
sabbir-bot-comunity <s33932334@gmail.com>
2026-04-10 16:29:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,56 +5,31 @@ const express = require('express');
 
 const app = express();
 const botName = "SABBIR CHAT BOT";
-
-
 const langFile = JSON.parse(fs.readFileSync("./languages/bn.json", "utf8"));
-
 
 const getText = (path, ...args) => {
   let text = path.split(".").reduce((obj, key) => obj?.[key], langFile) || path;
   args.forEach((arg, i) => text = text.replace(`%${i + 1}`, arg));
-  
-  if (path.startsWith("commands")) {
-      return `[ ${botName} ] : ${text}`;
-  }
-  return text;
+  return path.startsWith("commands") ? `[ ${botName} ] : ${text}` : text;
 };
 
-
-app.get('/', (req, res) => {
-  res.send(`${botName} IS RUNNING...`);
-});
-
-app.listen(3000, () => {
-  logger("Uptime Server রেডি (Port: 3000)!", "INFO");
-});
-
+app.get('/', (req, res) => res.send(`${botName} IS RUNNING...`));
+app.listen(3000, () => logger("Uptime Server রেডি!", "INFO"));
 
 module.exports = () => {
-  if (!fs.existsSync("./account.txt")) {
-    return logger(getText("login.noAppState"), "ERROR");
-  }
-
+  if (!fs.existsSync("./account.txt")) return logger(getText("login.noAppState"), "ERROR");
   const appState = JSON.parse(fs.readFileSync("./account.txt", "utf8"));
-  
+
   login({ appState }, (err, api) => {
     if (err) return logger(getText("login.error", err), "ERROR");
-
     api.setOptions({ listenEvents: true, selfListen: false, online: true });
     logger(getText("login.success"), "LOGIN");
 
     api.listenMqtt((err, event) => {
       if (err || !event.body) return;
-
-      const message = event.body.toLowerCase();
-
-      
-      if (message === "hi") {
-        api.sendMessage(getText("commands.hi"), event.threadID);
-      } 
-      else if (message === "prefix") {
-        api.sendMessage(getText("commands.prefix"), event.threadID);
-      }
+      const msg = event.body.toLowerCase();
+      if (msg === "hi") api.sendMessage(getText("commands.hi"), event.threadID);
+      if (msg === "prefix") api.sendMessage(getText("commands.prefix"), event.threadID);
     });
   });
 };
