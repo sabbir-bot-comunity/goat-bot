6ee69677e9495ab13faf66f16b5181c673638b0e
sabbir-bot-comunity <s33932334@gmail.com>
2026-04-10 16:28:51 +0600

    Update Sabbir.js

--- Sabbir.js
+++ Sabbir.js
@@ -1,9 +1,13 @@
 const startBot = require("./index.js");
+const fs = require("fs");
 
-console.log("🚀 Sabbir-Mirai-Bot শুরু হচ্ছে...");
+const botName = "SABBIR CHAT BOT";
+const lang = JSON.parse(fs.readFileSync("./languages/bn.json", "utf8"));
+
+console.log(`\x1b[36m[ ${botName} ]\x1b[0m \x1b[35m🚀 ${lang.system.starting}\x1b[0m`);
 
 try {
     startBot();
 } catch (error) {
-    console.error("বট চালু করতে সমস্যা হয়েছে:", error);
+    console.error(`\x1b[31m[ ${botName} ] ${lang.system.errorStarting}\x1b[0m`, error);
 }
